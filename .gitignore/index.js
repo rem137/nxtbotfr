89cb5a7e2314c76b5be68894e4567fd06e0b57f7
rem137 <56608633+rem137@users.noreply.github.com>
2020-03-28 22:45:19 +0100

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,175 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const monnaie = require('./money.json');
+const fs = require('fs')
+var dates = new Date();
+var conteur = dates.getMinutes();
+var msg;
+var ok = false;
+var okd = 1;
+var entier = -100;
+var number = 0;
+var question = ["hey","comment va tu?"];
+var reponse = ["salut"];
+var token = "NjMzNzA5NDAzOTM3MzA4Njcz.XnyHfw.dIOJ-x8UmoeVwrDn_PVV8qiJkSQ";
+var age = "3 jours";
+var msgCompt = 0;
+var lastmsg = "";
+var permission = false;
+msgSup = entierAleatoire(0,58);
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+function entierAleatoire(min, max)
+{
+ return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+//Utilisation
+//La variable contient un nombre aléatoire compris entre 1 et 10
+
+setInterval (function(){
+var date = new Date();
+var jour = date.getDay();
+var heure = date.getHours();
+var minutes = date.getMinutes();
+if (permission === true)
+{
+  question[question.length] = lastmsg;
+  reponse[reponse.length] = lastmsg;
+  permission = false;
+}
+if (minutes === msgSup + 1)
+ {
+  okd = 1;
+  msgSup = entierAleatoire(0, 58);
+}
+if (number === 9)
+{
+  client.login("NjkxMzE1Mjc5NzY1OTYyODUy.XnyHpQ.LEz0jx9h4_7-TsKFIvbfYIFawlQ").then(() => {
+    client.login(token);
+    number = 0;
+})}
+if (conteur >= 60)
+{
+conteur = conteur - 60;
+}
+if (conteur === 59)
+{
+  conteur = conteur + 1;
+}
+if (minutes === conteur + 1)
+{
+//client.channels.cache.get('691691656986099845').send(minutes);
+conteur = conteur + 1;
+number = number + 1;
+}
+if (minutes === msgSup)
+{
+  if (okd === 1)
+  {
+client.channels.cache.get('691691656986099845').send(question[entierAleatoire(0,question.length - 1)]);
+okd = okd + 1;
+}}})
+client.on('message', msg => {
+  if (!monnaie[msg.author.username])
+  {
+    monnaie[msg.author.username] = {
+      monnaie: 0
+    }
+  }
+  if (msg.content.includes("monnaie")) {
+    monnaie[msg.author.username] = {
+      monnaie: monnaie[msg.author.username].monnaie + 2
+    };
+  }
+  fs.writeFile('./money.json', JSON.stringify(monnaie), err =>{
+    if (err) console.log(err);
+  })
+  if (msg.author != 633709403937308673)
+  {
+  msgCompt++;
+  lastmsg = msg.content;
+  }
+var int = 0;
+  for (let N = 0; N < question.length;N++)
+  {
+    if (lastmsg != question[N])
+    {
+      int++;
+    }
+    if (int === (question.length))
+      {
+        permission = true;
+      }
+    if (msg.author != 633709403937308673)
+    {
+  if (msg.content === question[N])
+  {
+    msg.reply(reponse[N])
+  }}}
+  if (msg.content.includes("_")) {
+    question[question.length] = msg.content.substring(msg.content.length - (msg.content.length - msg.content.indexOf("¿")),msg.content.length);
+    question[question.length - 1] = msg.content.replace(question[question.length - 1],"");
+    question[question.length - 1] = question[question.length - 1].replace("_","");
+    reponse[reponse.length] = (msg.content.substring(msg.content.indexOf("¿")))
+    reponse[reponse.length - 1] = reponse[reponse.length - 1].replace("¿","")
+    msg.reply("message added")
+  }
+  if (msg.content.includes("va")||msg.content.includes("vas")||msg.content.includes("Va")||msg.content.includes("Vas"))
+  {
+    if (msg.content.includes("foutre"))
+    {
+      msg.reply("tg sale mioche")
+    }
+    if (msg.content.includes("dormir"))
+    {
+      msg.reply("je n'ai pas besoin de dormir")
+    }
+  }
+  if (msg.content.includes("tu as")||msg.content.includes("ta")||msg.content.includes("t’a")||msg.content.includes("Ta")||msg.content.includes("T a")||msg.content.includes("t a")||msg.content.includes("T’a"))
+  {
+    if (msg.content.includes("age")||msg.content.includes("âge"))
+    {
+      msg.reply("j'ai environs " + age)
+    }
+    if (msg.content.includes("faim"))
+    {
+      msg.reply("je ne peux pas manger.")
+    }
+  }
+  if (msg.content.includes("random number")) {
+    msg.reply(entierAleatoire(1,10))
+  }
+  if (msg.content.includes("msgSup")) {
+    msg.reply(msgSup)
+  }
+  if (msg.content.includes("msg")) {
+    msg.reply(msgCompt)
+  }
+  if (msg.content.includes("okd")) {
+    msg.reply(okd)
+  }
+  if (msg.content.includes("N")) {
+    msg.reply(msg.author)
+  }
+  if (msg.content.includes("Q"))
+  {
+    if (msg.author != 633709403937308673)
+    {
+    msg.reply(question[question.length - 1])
+  }}
+  if (msg.content.includes("number")) {
+    msg.reply(number)
+  }
+  if (msg.content.includes("restart")) {
+    msg.reply("rebooting...").then(m => {
+    client.login("NjkxMzE1Mjc5NzY1OTYyODUy.XntVLg._pm5fG9QFBiaXfB6bb7YSxL7xVM").then(() => {
+      client.login(token);
+  })})}
+  if (msg.content.includes("initial")) {
+    msg.author.send("ok");
+    msgSup = entierAleatoire(0,58);
+  }
+});
+
+client.login(token);
